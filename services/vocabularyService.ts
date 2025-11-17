d06908b5cacb2eb04b741daf998a4416123fed3e
Divyanshu Shukla <divyanshu.shukla@uneecops.in>
2025-11-17 16:13:55 +0530

    fix: update vocabulary fetching method to use new API structure
    Refactored the fetchVocabulary method in vocabularyService to align with the updated API endpoint and request parameters. This change ensures that vocabulary words are fetched correctly from the new API structure, improving data retrieval consistency.

--- services/vocabularyService.ts
+++ services/vocabularyService.ts
@@ -3,7 +3,8 @@ import serverInterfaceService from "./serverInterfaceService";
 export const vocabularyService = {
   // Fetch vocabulary words
   fetchVocabulary: (page: number = 1, pageSize: number = 10) =>
-    serverInterfaceService.get(
-      `/vocabulary?page=${page}&page_size=${pageSize}`
-    ),
+    serverInterfaceService.get(`/vocabulary/list`, {
+      page: 1,
+      page_size: 10,
+    }),
 };
